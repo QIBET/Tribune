009b23efd83f28c299c9f9b672100342b95b712a
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-02 10:52:33 +0300

    Updating allowed hosts

--- tribune/settings.py
+++ tribune/settings.py
@@ -41,7 +41,7 @@ django_heroku.settings(locals())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tribuneqibet.herokuapp.com']
 
 
 # Application definition
