26f83b5b03babb420461807d05194a838c60d8b1
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-02 11:17:49 +0300

    Update on settings

--- tribune/settings.py
+++ tribune/settings.py
@@ -102,10 +102,10 @@ if config('MODE')=="dev":
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
        }
        
    }
