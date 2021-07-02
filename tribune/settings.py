072aa5ce5e650ef2d0395260071601b081973379
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-02 11:05:12 +0300

    controls

--- tribune/settings.py
+++ tribune/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-try: 
-     config['MIDDLEWARE_CLASSES'] = tuple(['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE_CLASSES'])) 
-except KeyError: 
-     config['MIDDLEWARE'] = tuple(['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE'])) 
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
